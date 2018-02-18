dcb1f7dd9247986ced4e5dce5076d5a66f7a1a32
spsivalingam <ssubbiah@uncc.edu>
2018-02-18 11:50:05 -0500

    "employee"

--- src/main/java/uncc/api/uncc/api/Entity/Employee.java
+++ src/main/java/uncc/api/uncc/api/Entity/Employee.java
@@ -16,8 +16,8 @@ public class Employee extends User {
         this.address = address;
     }
 
-    public void setEmployeeId(String employeeId) {
-        this.employeeId = employeeId;
+    public void setEmployeeId(String empId) {
+        this.employeeId = empId;
     }
 
     public void setName(String name) {
