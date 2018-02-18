ed2d653bd86badc28abcf8d44515a56a479ebdc7
spsivalingam <ssubbiah@uncc.edu>
2018-02-18 11:07:08 -0500

    Included New Method

--- src/main/java/uncc/api/uncc/api/Repository/UserRepository.java
+++ src/main/java/uncc/api/uncc/api/Repository/UserRepository.java
@@ -3,11 +3,15 @@ package uncc.api.uncc.api.Repository;
 import org.springframework.data.repository.CrudRepository;
 import uncc.api.uncc.api.Entity.User;
 
+import java.util.Iterator;
+
 /**
  * Created by sivalingam on 2/10/2018.
  */
 public interface UserRepository extends CrudRepository<User,String> {
 
-    User findByuser_id(String user_id);
+    Iterator<User> findByuser_id(String user_id);
 
+    @Override
+    Iterable<User> findAll();
 }
